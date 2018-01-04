8fca07ec33c9fbd15d3167c011a925136028edac
Jurek Dörfler <jdorfler@pgs-soft.com>
2018-01-04 15:26:02 +0100

    Looks better

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import Footer from './modules/Footer.js';
 import Sidebar from './modules/Sidebar.js';
 import Main from './modules/Main.js'
 
+const archivesItems = [
+    'March 2014',
+    'February 2014',
+    'January 2014',
+    'December 2013',
+    'November 2013',
+    'October 2013',
+    'September 2013',
+    'August 2013',
+    'July 2013',
+    'June 2013',
+    'May 2013',
+    'April 2013'
+];
+
+const elsewhereItems = [
+    'GitHub',
+    'Twitter',
+    'Facebook'
+];
+
 class App extends Component {
     render() {
         return (
@@ -16,7 +37,7 @@ class App extends Component {
                 <main role="main" className="container">
                     <div className="row">
                         <Main/>
-                        <Sidebar/>
+                        <Sidebar archives={archivesItems} elsewhere={elsewhereItems}/>
                     </div>
                 </main>
 
--- src/modules/Main.js
+++ src/modules/Main.js
@@ -4,10 +4,7 @@ export default function Main() {
     return (
         <div className="col-sm-8 blog-main">
 
-            <div className="blog-post">
-                <h2 className="blog-post-title">Sample blog post</h2>
-                <p className="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
-
+            <BlogPost title="Sample blog post" date="January 1, 2014" author="Mark">
                 <p>This blog post shows a few different types of content that's supported and styled with Bootstrap.
                     Basic typography, images, and code are all supported.</p>
                 <hr/>
@@ -47,12 +44,9 @@ export default function Main() {
                 </ol>
                 <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
 
-            </div>
+            </BlogPost>
 
-            <div className="blog-post">
-                <h2 className="blog-post-title">Another blog post</h2>
-                <p className="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
-
+            <BlogPost title="Another blog post" date="December 23, 2013" author="Jacob">
                 <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus.
                     Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere
                     consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
@@ -65,11 +59,9 @@ export default function Main() {
                 <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo
                     luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac
                     consectetur ac, vestibulum at eros.</p>
-            </div>
+            </BlogPost>
 
-            <div className="blog-post">
-                <h2 className="blog-post-title">New feature</h2>
-                <p className="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
+            <BlogPost title="New feature" date="December 14, 2013" author="Chris">
 
                 <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia
                     bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus,
@@ -83,14 +75,35 @@ export default function Main() {
                 <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet
                     fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
                 <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-            </div>
+            </BlogPost>
 
-            <nav className="blog-pagination">
-                <a className="btn btn-outline-primary" href="#">Older</a>
-                <a className="btn btn-outline-secondary disabled" href="#">Newer</a>
-            </nav>
+            <Navbar/>
+
 
         </div>
     );
 
+}
+
+function Navbar() {
+    return (
+        <nav className="blog-pagination">
+            <a className="btn btn-outline-primary" href="#">Older</a>
+            <a className="btn btn-outline-secondary disabled" href="#">Newer</a>
+        </nav>
+    );
+}
+
+function BlogPost(props) {
+    const title = props.title;
+    const date = props.date;
+    const author = props.author;
+
+    return (
+        <div className="blog-post">
+            <h2 className="blog-post-title">{title}</h2>
+            <p className="blog-post-meta">{date} by <a href="#">{author}</a></p>
+            {props.children}
+        </div>
+    );
 }
