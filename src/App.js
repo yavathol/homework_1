2c7574e21880c2125857f79f52958c5661372f35
Jurek Dörfler <yavathol@gmail.com>
2018-01-17 23:46:48 +0100

    Fixed after comments from JT.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import Head from './modules/Head.js';
 import Header from './modules/Header.js';
 import Footer from './modules/Footer.js';
 import Sidebar from './modules/Sidebar.js';
@@ -8,10 +7,7 @@ import Main from './modules/Main.js'
 class App extends Component {
     render() {
         return (
-            <html lang="en">
             <div className="App">
-                <Head/>
-                <body>
                 <Header/>
                 <main role="main" className="container">
                     <div className="row">
@@ -19,11 +15,8 @@ class App extends Component {
                         <Sidebar/>
                     </div>
                 </main>
-
                 <Footer/>
-                </body>
             </div>
-            </html>
         );
     }
 }
